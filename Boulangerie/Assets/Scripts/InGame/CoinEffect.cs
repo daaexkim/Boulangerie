11efbfd612626188e393cdca7310e4f07324699f
daaexkim <jennyxdaae@gachon.ac.kr>
2024-12-06 18:43:48 +0900

    0.8
    - 번역 완성
    - 게임 오버 조건 업그레이드
    - bebe 국기 숨김
    - 빌드 준비
    - burned-bited pain 추가
    - 게임 설명 수정
    - textmeshpro 수정

---
+++ Boulangerie/Assets/Scripts/InGame/CoinEffect.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Redcode.Pools;
+using DG.Tweening;
+
+public class CoinEffect : MonoBehaviour, IPoolObject
+{
+    SpawnManager sm;
+    GameManager gm;
+
+    public void OnCreatedInPool()
+    {
+        sm = SpawnManager.Instance;
+        gm = GameManager.Instance;
+
+        name = name.Replace("(Clone)", "");
+    }
+
+    public void OnGettingFromPool()
+    {
+    }
+
+    public void MoveTween_IsPlus(Vector2 startPos, Vector2 targetPos)
+    {
+        transform.position = startPos;
+
+        Vector2 spawnPos = startPos + Random.insideUnitCircle * 100f;
+
+        Sequence moveSeq = DOTween.Sequence().SetUpdate(true);
+        moveSeq.Append(transform.DOMove(spawnPos, 1f).SetEase(Ease.OutQuart))
+            .Join(transform.DOShakeScale(1f, 1, 10, 1))
+            .Append(transform.DOMove(targetPos, 1f))
+            .OnComplete(() => {
+                sm.Destroy_CoinEffect(this);
+                gm.GainCoin(10);
+            });
+    }
+    public void MoveTween_IsMinus(Vector2 startPos, Vector2 targetPos)
+    {
+        transform.position = startPos;
+
+        Vector2 spawnPos = startPos + Random.insideUnitCircle * 100f;
+
+        Sequence moveSeq = DOTween.Sequence().SetUpdate(true);
+        moveSeq.OnStart(() => gm.GainCoin(-10));
+        moveSeq.Append(transform.DOMove(spawnPos, 1f).SetEase(Ease.OutQuart))
+            .Join(transform.DOShakeScale(1f, 1, 10, 1))
+            .Append(transform.DOMove(targetPos, 1f))
+            .OnComplete(() => {
+                sm.Destroy_CoinEffect(this);
+            });
+    }
+}
--- 블랑제리/Assets/Scripts/Managers/UIManager_mm.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIManager_mm : Singleton<UIManager_mm>
-{
-}
